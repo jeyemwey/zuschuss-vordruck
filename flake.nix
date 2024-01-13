7a6274ab7c1c6a619e79edc504a4b574fcc912d3
Jannik <jannik@outlook.com>
2024-01-13 22:36:27 +0100

    :green_heart: Fix flake

--- flake.nix
+++ flake.nix
@@ -2,19 +2,24 @@
   description = "A flake for building Hello World";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-  # inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = {self, nixpkgs}: let
-      lib = nixpkgs.lib;
-      pkgs = nixpkgs.legacyPackages."aarch64-darwin";
-    in {
-      packages."aarch64-darwin".default = pkgs.php.buildComposerProject {
-        src = ./.;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = nixpkgs.lib;
+      in {
+        packages.default = pkgs.php.buildComposerProject {
+          src = builtins.filterSource (path: type:
+            !(type == "directory" && (baseNameOf path == "vendor"
+              || baseNameOf path == ".idea" || baseNameOf path == "result")))
+            ./.;
 
-        pname = "zuschuss-vordruck";
-        version = "1.0.0";
+          pname = "zuschuss-vordruck";
+          version = "1.0.0";
 
-        vendorHash = "sha256-MHE7FUHRSz/WckvW3MiSR/CcZrr99jebEuPLXSTunmo=";
-      };
-  };
+          vendorHash = "sha256-MHE7FUHRSz/WckvW3MiSR/CcZrr99jebEuPLXSTunmo=";
+        };
+      });
 }
